8c6ab7da43d12a09da5d535dfd63948014972124
zhanglinling <zhanglinling0926@gmail.com>
2017-06-19 22:25:52 +0800

    修改错误秘钥

--- minicourse-ajax-project-2/js/script.js
+++ minicourse-ajax-project-2/js/script.js
@@ -11,14 +11,20 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
     // load streetview
-    var streerStr = $("#street").val();
-    var cityStr = $("#city").val();
-    $("body").append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + streerStr+ "," + cityStr + '">');
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     // YOUR CODE GOES HERE!
-    var nytimesKey = "AIzaSyAcKuNWvchfVg45w8ZLy8HXE7eieDOI-uE";
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&sort=newest&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=" + nytimesKey;
+    var nytimesKey = "993d4f8bcdce4356a26d65675ae3c845";
+    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&sort=newest&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=" + nytimesKey;
     $.getJSON(url, function(data) {
         console.log(data);
     });
